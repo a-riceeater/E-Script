02507fd2cb98e694ed785884b182accdff2ba69b
Elijah <46640763+ghwosty@users.noreply.github.com>
2023-01-11 11:04:45 -0800

    add base

--- src/escript.js
+++ src/escript.js
@@ -2,3 +2,11 @@
 E-Script
 An API that makes programming in JavaScript very easy.
 */
+
+const eScriptApi;
+
+eScriptApi.prototype.addRaw = function(injection) {
+    const esaElement = document.createElement("eScriptApiElement");
+    esaElement.innerHTML = "ello"
+    document.body.append(esaElement);
+}
